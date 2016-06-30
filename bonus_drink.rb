8744b931b45b71a2e8330fcde6150dc6128a31e1
na777 <na4477g@gmail.com>
2016-06-30 19:51:22 +0900

    my_answer

--- bonus_drink.rb
+++ bonus_drink.rb
@@ -1,5 +1,17 @@
 class BonusDrink
   def self.total_count_for(amount)
-    0
+    total = amount
+    while amount >= 3 do
+      total += amount / 3 
+      amount = ( amount / 3 ) + amount % 3
+    end
+    return total
   end
+end
+
+if __FILE__ == $0
+  p BonusDrink.total_count_for(1)
+  p BonusDrink.total_count_for(3)
+  p BonusDrink.total_count_for(11)
+  p BonusDrink.total_count_for(100)
 end
